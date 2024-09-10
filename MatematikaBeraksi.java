e94e7f80e9a0df92d5553c244e8699d90c936384
cynpardede <Cynthiapards@gmail.com>
2024-09-10 22:21:07 +0700

    Add files via upload

---
+++ MatematikaBeraksi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author CINDY
+ */
+public class MatematikaBeraksi {
+    public static void main(String[] args) {
+        Matematika matematika = new Matematika();
+        
+        System.out.println("pertambahan : 20 + 10 = " + matematika.pertambahan(20,10));
+        System.out.println("pengurangan : 10 - 5 = " + matematika.pengurangan(10,5));
+        System.out.println("perkalian : 10 * 3 = " + matematika.perkalian(10,3));
+        System.out.println("pembagian : 21 / 2 = " + matematika.pembagian(21,2));
+              
+    }   
+}
---
+++ MatematikaInterface.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author CINDY
+ */
+public interface MatematikaInterface {
+    int pertambahan(int a, int b);
+    int pengurangan(int a, int b);
+    int perkalian(int a, int b);
+    int pembagian(int a, int b);
+}
